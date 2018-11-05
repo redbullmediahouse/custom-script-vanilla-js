3e529f7f84da7b34f1c70275486610be1ecfed43
robertmaier <robert.maier@redbull.com>
2018-11-05 13:29:17 +0100

    RBMN-11709 adds example for renderInlineVideoPlayer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 function start(params) {
     const {el, config, options} = params;
-    const {resolveTranslation, resolveImageUrl} = options;
+    const {resolveTranslation, resolveImageUrl, renderInlineVideoPlayer} = options;
 
     const wrapper = document.createElement('div');
     wrapper.className = 'custom-class';
@@ -18,35 +18,52 @@ function start(params) {
     title.innerText = resolveTranslation(config.caption, 'Hello World!');
     wrapper.appendChild(title);
 
-    const image = document.createElement('img');
-    /*
-        resolveImageUrl takes two arguments.
-        1. A content repository endpoint pointing to an image asset. The required `rb3Schema=v1:image` is added if not provided.
-        2. An options object with the following possibilities:
-            width: The desired width of the image (required).
-            and one of the following:
-            - aspectRatio: The aspect ratio as number, e.g. 1.78 for 16/9 (calculated as width / height)
-            - height: The desired height of the image
+    const videoEl = document.createElement('div');
+    videoEl.className = 'example-video';
 
-            NOTE: The width, height and aspectRatio parameters only work for images that do have an imageProvider other than 'absolute',
-            as the transformation is done on the image server, not on the client.
+    const imageEl = document.createElement('img');
 
-            Examples:
-            resolveImageUrl(imageEndpoint, {width: 500, aspectRatio: 1.78});
-            resolveImageUrl(imageEndpoint, {width: 500, height: 200});
-     */
     const exampleImageEndpoint = '/v3/api/content/v1/images/8bcd3825-cb43-4a91-8e56-c1cddd55ff33/en-INT';
-    return resolveImageUrl(exampleImageEndpoint, {width: 400, aspectRatio: 1}).then(imageSrc => {
-        image.setAttribute('src', imageSrc);
-        wrapper.appendChild(image);
+    const exampleVideoEndpoint = '/v3/api/content/v1/videos/cdef73a0-5f75-4d4f-bb0e-aaca6275fd6e/en-INT';
+
+    return Promise.all([
+        /*
+           resolveImageUrl takes two arguments.
+           1. A content repository endpoint pointing to an image asset. The required `rb3Schema=v1:image` is added if not provided.
+           2. An options object with the following possibilities:
+               width: The desired width of the image (required).
+               and one of the following:
+               - aspectRatio: The aspect ratio as number, e.g. 1.78 for 16/9 (calculated as width / height)
+               - height: The desired height of the image
+
+               NOTE: The width, height and aspectRatio parameters only work for images that do have an imageProvider other than 'absolute',
+               as the transformation is done on the image server, not on the client.
+
+               Examples:
+               resolveImageUrl(imageEndpoint, {width: 500, aspectRatio: 1.78});
+               resolveImageUrl(imageEndpoint, {width: 500, height: 200});
+        */
+        resolveImageUrl({imageEndpoint: exampleImageEndpoint, options: {width: 400, aspectRatio: 1}}).then(imageSrc => imageEl.setAttribute('src', imageSrc)),
+        /*
+            renderInlineVideoPlayer takes 3 arguments.
+            1. A content repository endpoint pointing to a video asset. The required `rb3Schema=v1:video` is added if not provided.
+            2. A content repository endpoint loading a playlist. Not required for a single video.
+            3. The DOM element the video player should be added to
+         */
+        renderInlineVideoPlayer(exampleVideoEndpoint, null, videoEl)
+    ]).then(() => {
+        wrapper.appendChild(imageEl);
+        wrapper.appendChild(videoEl);
         el.appendChild(wrapper);
         console.log('started');
-    }).then(() => ({
-        stop: () => {
-            console.log('stopped');
-            return Promise.resolve();
-        }
-    }));
+
+        return {
+            stop: () => {
+                console.log('stopped');
+                return Promise.resolve();
+            }
+        };
+    });
 }
 
 export {start};
