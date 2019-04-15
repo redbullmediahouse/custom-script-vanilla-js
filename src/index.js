d02afc908700f57913bbb49cc007084dcc500320
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2019-04-15 10:38:44 +0200

    fixes resolveImageUrl documentation

--- src/index.js
+++ src/index.js
@@ -28,9 +28,9 @@ function start(params) {
 
     return Promise.all([
         /*
-           resolveImage takes two arguments.
-           1. A content repository endpoint pointing to an image asset. The required `rb3Schema=v1:image` is added if not provided.
-           2. An options object with the following possibilities:
+           resolveImage takes one arguments.
+           1. An options object with the following possibilities:
+               imageEndpoint: A content repository endpoint pointing to an image asset (e.g. /v3/api/content/v1/images/8bcd3825-cb43-4a91-8e56-c1cddd55ff33/en-INT)
                width: The desired width of the image (required).
                and one of the following:
                - aspectRatio: The aspect ratio as number, e.g. 1.78 for 16/9 (calculated as width / height)
@@ -40,8 +40,8 @@ function start(params) {
                as the transformation is done on the image server, not on the client.
 
                Examples:
-               resolveImageUrl(imageEndpoint, {width: 500, aspectRatio: 1.78});
-               resolveImageUrl(imageEndpoint, {width: 500, height: 200});
+               resolveImageUrl({imageEndpoint: "/v3/api/content/v1/images/8bcd3825-cb43-4a91-8e56-c1cddd55ff33/en-INT", width: 500, aspectRatio: 1.78});
+               resolveImageUrl({imageEndpoint: "/v3/api/content/v1/images/8bcd3825-cb43-4a91-8e56-c1cddd55ff33/en-INT", width: 500, height: 200});
         */
         resolveImageUrl({imageEndpoint: exampleImageEndpoint, options: {width: 400, aspectRatio: 1}}).then(({image, imageUrl}) => {
             imageEl.setAttribute('src', imageUrl);
